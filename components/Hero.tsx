888a095fb2918653d57be7777da7671bb62089b4
ayazwx <vefaayazwx@gmail.com>
2023-07-30 20:59:49 +0300

    bug fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
 
   return (
     <div className="hero">
-      <div className="flex-1 pt-36-padding-x">
+      <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
           Find, book, rent a car—quick and super easy!
         </h1>
